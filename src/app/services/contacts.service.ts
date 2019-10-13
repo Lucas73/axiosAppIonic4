2ceade89c4972ffe7ed848220f9e501f3a5910c3
Lucas <lucas@MacBook-Air-de-Lucas.local>
2019-10-13 11:02:34 +0200

    working

--- src/app/services/contacts.service.ts
+++ src/app/services/contacts.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import axios from 'axios';
+import { Observable, of } from 'rxjs';
+import { Contact } from '../models/Contact';
+import { ɵBROWSER_SANITIZATION_PROVIDERS } from '@angular/platform-browser';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactsService {
 
-  constructor() { }
+  constructor() { 
+    axios.defaults.baseURL = "https://reqres.in/api";
+   }
+
+   obtainAllContacts(page?:string):Observable<Contact[]>{
+     let url = '/users'
+
+     if(page){
+       url = url.concat('?page=').concat(page)
+     }
+
+     let observable = Observable.create( (observer) => {
+       axios.get(url)
+        .then( ( response ) => {
+          observer.next (response.data.data);
+          observer.complete();
+        })
+        .catch( ( error ) => {
+          observer.error(error);
+        });
+     }
+     );
+     return observable;
+   }
 }
